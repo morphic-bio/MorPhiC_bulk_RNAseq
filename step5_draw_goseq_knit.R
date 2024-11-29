574fe2878d697a2e290e680d811aab2d7a42e867
liusi2019 <liusi2019@gmal.com>
2024-11-28 20:53:42 -0800

    For 7 JAX datasets, save out volcano plots and goseq plots for PTC vs WT comparisons

--- step5_draw_goseq_knit.R
+++ step5_draw_goseq_knit.R
@@ -13,11 +13,12 @@ render_report = function(release_name) {
 rs = c("2023_12_JAX_RNAseq_ExtraEmbryonic",
        "2024_05_JAX_RNAseq2_ExtraEmbryonic",
        "2024_06_JAX_RNAseq_Neuroectoderm",
-       "2024_06_JAX_RNAseq4_ExEm_brain", 
+       "2024_06_JAX_RNAseq4_ExEm_brain",
        "2024_09_JAX_RNAseq5_ExE_CBO",
        "2024_09_JAX_RNAseq6_Diverse",
        "2024_09_JAX_RNAseq7_Reversion",
-       "2024_09_UCSF_Bulk_EB_Timecourse")
+       "2024_09_UCSF_Bulk_EB_Timecourse"
+       )
 
 
 for(r1 in rs){
