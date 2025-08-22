9b2806bdc9d9de4621377db524c2d2aa507f7e03
Bo Yu <byu2@fredhutch.org>
2025-08-22 12:59:30 -0700

    Create png volcano plots

--- step5_draw_goseq_2025_06_JAX_RNAseq_knit.R
+++ step5_draw_goseq_2025_06_JAX_RNAseq_knit.R
@@ -4,7 +4,7 @@ Sys.setenv(RSTUDIO_PANDOC="/Applications/RStudio.app/Contents/MacOS/pandoc")
 render_report = function(release_name) {
 
   rmarkdown::render(
-    "step5_draw_goseq.Rmd", 
+    "step5_draw_goseq_2025_06_JAX_RNAseq.Rmd", 
     params = list(release_name = release_name),
     output_file = paste0("step5_draw_goseq/step5_draw_goseq_", release_name, ".html")
   )
